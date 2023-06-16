077fae5ac545d6adb9aca76322d5de4008fb4743
drkoop <dr.koop.development@gmail.com>
2023-06-16 11:30:18 -0600

    viernes2

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,3 +1,11 @@
+import { getGuitarras } from "~/models/guitarras.server"
+
+
+export async function loader(){
+  const guitarras = await getGuitarras()
+  return guitarras
+}
+
 
 const Tienda = () => {
   return (
